944296a3397e654b1e80f5f70c8a57e8c62d18bf
Christian Krullikowski <christian.krullikowski@ororatech.com>
2024-12-09 08:08:46 +0100

    Quit properly

--- mahjong/main.py
+++ mahjong/main.py
@@ -1,3 +1,4 @@
+import sys
 from collections.abc import Generator
 
 import pygame
@@ -6,26 +7,7 @@ from positioner import NoneTouching
 
 GREY = (200, 200, 200)
 RED = (255, 0, 0)
-
-
-class Game:
-    def __init__(self) -> None:
-        pygame.init()
-
-    def __enter__(self) -> "Game":
-        return self
-
-    def __exit__(self, *args) -> None:
-        pygame.quit()
-
-    def event_loop(self, running: bool = True) -> Generator[bool, None, None]:
-        """Event loop handling user interactions."""
-        while running:  # game still running
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:  # if quit button is pressed
-                    running = False  # stop the game
-
-            yield running
+GREEN = (0, 255, 0)
 
 
 class Tiles:
@@ -75,13 +57,56 @@ class Screen:
         self.screen.blit(tile.face, tile.outline)
 
 
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+
+    def __enter__(self) -> "Game":
+        return self
+
+    def __exit__(self, *args) -> None:
+        pygame.quit()
+
+    def close(self) -> None:
+        pygame.quit()
+        sys.exit()
+
+    def event_loop(self, running: bool = True) -> Generator[bool, None, None]:
+        """Event loop handling user interactions."""
+        while running:  # game still running
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:  # if quit button is pressed
+                    running = False  # stop the game
+
+                yield event
+
+
+class Event(Game):
+    def __init__(self, event) -> None:
+        self.event: pygame.event.Event = event
+
+    def check_event_type(self, tile) -> None:
+        if self.event.type == pygame.QUIT:  # if quit button is pressed
+            self.close()  # stop the game
+
+        if self.event.type == pygame.MOUSEBUTTONDOWN:  # if mouse button is pressed
+            x, y = self.event.pos  # get x and y position of mouse click
+
+            if tile.outline.collidepoint(x, y):
+                print(f"Tile {tile.id} clicked")
+                tile.color = GREEN
+
+
 if __name__ == "__main__":
     with Game() as game:
         screen = Screen()
         tiles = Tiles()
 
-        for _ in game.event_loop():
+        for e in game.event_loop():
+            event = Event(e)
             screen.draw_background()
             for tile in tiles.tiles:
                 screen.draw_tile(tile)
+                event.check_event_type(tile)
+
             pygame.display.update()  # update the screen to show the color
